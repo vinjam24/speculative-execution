a80d2299ef42d3b04f595790a6943a58d11a99d4
Raviteja Vinjam <ravitejavinjam21@gmail.com>
2024-04-28 20:46:12 -0400

    added cache in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,13 @@ using namespace std;
 int main() {
     Speculator speculator;
     speculator.create_speculation();
+    Cache<std::string, std::string> stringCache(100);
+    stringCache.put("key1", "value1");
+    stringCache.put("key2", "value2");
+
+    std::string value;
+    if (stringCache.get("key1", value)) {
+        std::cout << "Value of key 'key1': " << value << std::endl;
+    }
     return 0;
 }
